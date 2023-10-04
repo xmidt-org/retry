edda7be3720d3af7c1c40761f7f619b60795d420
John Bass <johnbass1017@yahoo.com>
2023-10-04 15:47:27 -0700

    chore: delint

--- retryhttp/mocks_test.go
+++ retryhttp/mocks_test.go
@@ -9,16 +9,19 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+//nolint:unused
 type mockHTTPClient struct {
 	mock.Mock
 }
 
+//nolint:unused
 func (m *mockHTTPClient) Do(request *http.Request) (*http.Response, error) {
 	args := m.Called(request)
 	response, _ := args.Get(0).(*http.Response)
 	return response, args.Error(1)
 }
 
+//nolint:unused
 func (m *mockHTTPClient) ExpectDo(request *http.Request, response *http.Response, err error) *mock.Call {
 	return m.On("Do", request).Return(response, err)
 }
