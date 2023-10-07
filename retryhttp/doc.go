6316b40ecefc9b166c28dfb57391450087052f6e
John Bass <johnbass1017@yahoo.com>
2023-10-07 13:43:48 -0700

    chore: removed HTTP client semantics for the initial release

--- retryhttp/doc.go
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Comcast Cable Communications Management, LLC
-// SPDX-License-Identifier: Apache-2.0
-
-/*
-Package retryhttp provides simple tasks around HTTP client operations.
-*/
-package retryhttp
