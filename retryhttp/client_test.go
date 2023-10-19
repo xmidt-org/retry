aaf71984d187293f8883600053b3eff39f4e44e1
John Bass <johnbass1017@yahoo.com>
2023-10-18 17:20:55 -0700

    chore: initial client unit tests

--- retryhttp/client_test.go
+++ retryhttp/client_test.go
@@ -1,29 +1,149 @@
 package retryhttp
 
 import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/suite"
+	"github.com/xmidt-org/retry"
 )
+
+var testResponse = []byte("test response")
+
+type handlerState struct {
+	lock             sync.Mutex
+	expectedAttempts int
+	attempts         int
+}
+
+func (hs *handlerState) resetAttempts(expectedAttempts int) {
+	defer hs.lock.Unlock()
+	hs.lock.Lock()
+
+	hs.expectedAttempts = expectedAttempts
+	hs.attempts = 0
+}
+
+func (hs *handlerState) onAttempt() (done bool) {
+	defer hs.lock.Unlock()
+	hs.lock.Lock()
+
+	hs.attempts++
+	if hs.attempts >= hs.expectedAttempts {
+		done = true
+	}
+
+	return
+}
 
 type ClientSuite struct {
 	suite.Suite
+	handlerState
+
+	server *httptest.Server
 }
 
-func (suite *ClientSuite) timerStop() bool {
-	return true
+func (suite *ClientSuite) SetupSuite() {
+	suite.server = httptest.NewServer(suite)
 }
 
-// timer is a retry.Timer that returns a time channel that is immediately signaled.
-func (suite *ClientSuite) timer(d time.Duration) (<-chan time.Time, func() bool) {
-	suite.True(d > 0)
-	ch := make(chan time.Time, 1)
-	ch <- time.Now()
-	return ch, suite.timerStop
+func (suite *ClientSuite) TearDownSuite() {
+	suite.server.Close()
 }
 
-func (suite *ClientSuite) TestDo() {
+func (suite *ClientSuite) newRunner(opts ...retry.RunnerOption[*http.Response]) retry.Runner[*http.Response] {
+	runner, err := retry.NewRunner(opts...)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(runner)
+	return runner
+}
+
+func (suite *ClientSuite) newClient(opts ...ClientOption) *Client {
+	c, err := NewClient(opts...)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(c)
+	return c
+}
+
+func (suite *ClientSuite) newTestGet() *http.Request {
+	request, err := http.NewRequest("GET", suite.server.URL+"/test", nil)
+	suite.Require().NoError(err)
+	suite.Require().NotNil(request)
+	return request
+}
+
+func (suite *ClientSuite) newTestPut() *http.Request {
+	request, err := http.NewRequest("PUT", suite.server.URL+"/test", strings.NewReader("test"))
+	suite.Require().NoError(err)
+	suite.Require().NotNil(request)
+	return request
+}
+
+func (suite *ClientSuite) assertSuccess(response *http.Response, err error) {
+	suite.NoError(err)
+	suite.Require().NotNil(response)
+	suite.Equal(http.StatusOK, response.StatusCode)
+
+	var o bytes.Buffer
+	io.Copy(&o, response.Body)
+	response.Body.Close()
+	suite.Equal(testResponse, o.Bytes())
+}
+
+// ServeHTTP lets this suite be an http.Handler that returns a series of http.StatusServiceUnavailable
+// results followed by a final http.StatusOK.  The number of unavailable codes returned is managed
+// by the handler state.
+func (suite *ClientSuite) ServeHTTP(rw http.ResponseWriter, request *http.Request) {
+	if suite.onAttempt() {
+		rw.WriteHeader(http.StatusOK)
+	} else {
+		rw.WriteHeader(http.StatusServiceUnavailable)
+	}
+
+	rw.Write(testResponse)
+}
+
+func (suite *ClientSuite) TestGet() {
+	suite.Run("Default", func() {
+		c := suite.newClient()
+		suite.resetAttempts(1)
+		suite.assertSuccess(c.Do(suite.newTestGet()))
+	})
+
+	suite.Run("WithRequesters", func() {
+		c := suite.newClient(
+			WithRunner(
+				suite.newRunner(
+					retry.WithPolicyFactory[*http.Response](
+						retry.Config{
+							Interval: 5 * time.Second, // won't matter due to the immediate timer
+						},
+					),
+					retry.WithOnAttempt(CleanupResponse),
+					WithShouldRetry(http.StatusServiceUnavailable),
+					retry.WithImmediateTimer[*http.Response](),
+				),
+			),
+			WithRequesters(
+				func(request *http.Request) *http.Request {
+					request.Header.Set("Test", "true")
+					return request
+				},
+			),
+		)
+
+		suite.resetAttempts(3)
+		suite.assertSuccess(c.Do(suite.newTestGet()))
+	})
+
+	suite.Run("WithCustomClient", func() {
+	})
 }
 
 func TestClient(t *testing.T) {
